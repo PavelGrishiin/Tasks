f7a189a6c5bf272d38e46b5e66f4d5ae9fa2edd0
PavelGrishiin <iappleman333@gmail.com>
2022-04-30 19:36:08 +0300

    Make first task

--- Tasks/Tasks/Tasks/ToExecute/SliceStringOnUniqueSubstringsTask.swift
+++ Tasks/Tasks/Tasks/ToExecute/SliceStringOnUniqueSubstringsTask.swift
@@ -15,15 +15,61 @@ import Foundation
 // 4. Print final array
 
 class SliceStringOnUniqueSubstringsTask: TitleProvidableTask {
-	var title: String { "Slice string on unique substrings with length not more than input integer parameter" }
-	
-	func execute() {
-		// [0] - think about abstraction
-		// [1] - print task requirement
-		// [2] - execute task
-		// [3] - print execution result
-
-		print("SliceStringOnUniqueSubstringsTask execution...")
-		print(Texts.emptyLine)
-	}
+    var title: String { "Slice string on unique substrings with length not more than input integer parameter" }
+    
+    func execute() {
+        // [0] - think about abstraction
+        // [1] - print task requirement
+        // [2] - execute task
+        // [3] - print execution result
+        let string = "abcabcabcabcabcabcabcabcabcabcabcabcabacabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+        var sizeSupstring = 0
+        var result:Set = [""]
+        func inputStrigSize() {
+             print("please input number")
+             let inputSizeString = readLine()
+             guard let inputSizeString = inputSizeString else {
+                   return
+             }
+             guard let inputSizeString = Int(inputSizeString) else {
+                return inputStrigSize()
+             }
+        sizeSupstring = inputSizeString
+        }
+        
+        inputStrigSize()
+        
+        let auxiliaryVarible = sizeSupstring
+        var secondAuxiliaryVarible = 0
+        
+        func examinationSizeSubstring() {
+              if sizeSupstring <= 0{
+                 inputStrigSize()
+                 examinationSizeSubstring()
+              }
+              if sizeSupstring > string.count {
+                inputStrigSize()
+                examinationSizeSubstring()
+              }
+        }
+        
+        examinationSizeSubstring()
+        
+            let array = Array(string)
+            while array.count >= sizeSupstring {
+                while sizeSupstring < array.count {
+                    let text = String(array[secondAuxiliaryVarible...sizeSupstring - 1])
+                    secondAuxiliaryVarible =  sizeSupstring
+                    sizeSupstring = sizeSupstring + auxiliaryVarible
+                    result.insert(text)
+                }
+                result.insert(String(array[secondAuxiliaryVarible...array.count - 1]))
+            }
+        
+            
+        print("SliceStringOnUniqueSubstringsTask execution...")
+        print(Texts.emptyLine)
+        print(result)
+    }
+    
 }
