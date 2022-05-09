a6679822d56428f0e076f204db3bccff0d528d23
PavelGrishiin <iappleman333@gmail.com>
2022-05-09 18:38:43 +0300

    Completed the first task

--- Tasks/Tasks/Tasks/ToExecute/SliceStringOnUniqueSubstringsTask.swift
+++ Tasks/Tasks/Tasks/ToExecute/SliceStringOnUniqueSubstringsTask.swift
@@ -16,60 +16,63 @@ import Foundation
 
 class SliceStringOnUniqueSubstringsTask: TitleProvidableTask {
     var title: String { "Slice string on unique substrings with length not more than input integer parameter" }
-    
+    var taskString = "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
     func execute() {
-        // [0] - think about abstraction
-        // [1] - print task requirement
-        // [2] - execute task
-        // [3] - print execution result
-        let string = "abcabcabcabcabcabcabcabcabcabcabcabcabacabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
-        var sizeSupstring = 0
-        var result:Set = [""]
-        func inputStrigSize() {
-             print("please input number")
-             let inputSizeString = readLine()
-             guard let inputSizeString = inputSizeString else {
-                   return
-             }
-             guard let inputSizeString = Int(inputSizeString) else {
-                return inputStrigSize()
-             }
-        sizeSupstring = inputSizeString
+        print("""
+              You have one string variable
+               1. You need to write program with one input parameter of integer type
+               2. Than you need to slice string on substrings with length equals input parameter
+               3. After this you need to create array of unique substring from initial string
+               4. Print final array
+              
+              """)
+        numberOfElementsInSubstring()
+        sliceStringToSomeSubstring()
+   }
+    var inputNumberOfElementsInParts:Int = 0
+    func numberOfElementsInSubstring() -> Int {
+        let input = readNumberInput()
+        
+        switch input {
+        case .valid(let number):
+            return number
+        case .invalid(let message):
+            Console.Print.invalidInput(message: message)
+            return numberOfElementsInSubstring()
         }
         
-        inputStrigSize()
-        
-        let auxiliaryVarible = sizeSupstring
-        var secondAuxiliaryVarible = 0
-        
-        func examinationSizeSubstring() {
-              if sizeSupstring <= 0{
-                 inputStrigSize()
-                 examinationSizeSubstring()
-              }
-              if sizeSupstring > string.count {
-                inputStrigSize()
-                examinationSizeSubstring()
-              }
-        }
-        
-        examinationSizeSubstring()
-        
-            let array = Array(string)
-            while array.count >= sizeSupstring {
-                while sizeSupstring < array.count {
-                    let text = String(array[secondAuxiliaryVarible...sizeSupstring - 1])
-                    secondAuxiliaryVarible =  sizeSupstring
-                    sizeSupstring = sizeSupstring + auxiliaryVarible
-                    result.insert(text)
-                }
-                result.insert(String(array[secondAuxiliaryVarible...array.count - 1]))
-            }
-        
-            
-        print("SliceStringOnUniqueSubstringsTask execution...")
-        print(Texts.emptyLine)
-        print(result)
     }
     
+    func readNumberInput() -> NumberInput {
+        let inputTitleTexts = [Texts.Tasks.amountOfElements]
+        let inputString = Console.readInput(titleTexts: inputTitleTexts)
+        let input = validateNumberInput(fromString: inputString)
+        
+        return input
+    }
+    
+    func validateNumberInput(fromString inputString: String?) -> NumberInput {
+        var taskNumbersRange: ClosedRange<Int> { 1...taskString.count}
+
+        guard let inputString = inputString,
+              let inputNumber = Int(inputString),
+              taskNumbersRange ~= inputNumber else {
+            return .invalid(message: Texts.Errors.invalidNumberInput)
+        }
+        inputNumberOfElementsInParts = inputNumber
+        return .valid(number: inputNumber)
+    }
+    
+    func sliceStringToSomeSubstring() {
+        var convertTaskStringToArray = Array(taskString)
+        let numberOfParts = taskString.count / inputNumberOfElementsInParts
+        let x = inputNumberOfElementsInParts
+        while inputNumberOfElementsInParts <= taskString.count + numberOfParts {
+              convertTaskStringToArray.insert(" ", at: inputNumberOfElementsInParts)
+              inputNumberOfElementsInParts = inputNumberOfElementsInParts + x + 1
+        }
+        let result = Set(convertTaskStringToArray.split(separator: " "))
+        print("result Task")
+        print(result)
+    }
 }
